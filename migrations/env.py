db84ae55d694bcf54696e1d33964844baaa76d8b
1hundr3d <1hunnndr3d@gmail.com>
2026-05-21 12:36:16 +0000

    feat: env url

--- migrations/env.py
+++ migrations/env.py
@@ -1,3 +1,5 @@
+import os
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -16,6 +18,10 @@ from database import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+db_url = os.getenv("DATABASE_URL")
+if db_url:
+    config.set_main_option("sqlalchemy.url", db_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
